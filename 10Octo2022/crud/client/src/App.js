227de23c07e14fb6cf47de674b07315251bb6688
kavita.tambe <kavita.tambe@yash.com>
2022-10-11 11:16:09 +0530

    react and node app

---
+++ 10Octo2022/crud/client/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import Navbaar from "./components/Navbaar";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Edit from "./components/Edit";
+import Details from "./components/Details";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Navbaar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/edit/:id" element={<Edit />} />
+        <Route exact path="/view/:id" element={<Details />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
